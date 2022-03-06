bc431fbff9d63bef24a1c08d00472df80e41ddc3
Arpit Jindal <arpit.jindal@soprasteria.com>
2022-03-06 16:18:34 +0530

    updated versions

--- kafka-avro-v1/src/main/java/com/jindalarpit/kafka/apps/v1/KafkaAvroJavaProducerV1Demo.java
+++ kafka-avro-v1/src/main/java/com/jindalarpit/kafka/apps/v1/KafkaAvroJavaProducerV1Demo.java
@@ -12,13 +12,13 @@ public class KafkaAvroJavaProducerV1Demo {
     public static void main(String[] args) {
         Properties properties = new Properties();
         // normal producer
-        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        properties.setProperty("bootstrap.servers", "localhost:9092");
         properties.setProperty("acks", "all");
         properties.setProperty("retries", "1");
         // avro part
         properties.setProperty("key.serializer", StringSerializer.class.getName());
         properties.setProperty("value.serializer", KafkaAvroSerializer.class.getName());
-        properties.setProperty("schema.registry.url", "http://127.0.0.1:8081");
+        properties.setProperty("schema.registry.url", "http://localhost:8081");
 
         Producer<String, Customer> producer = new KafkaProducer<String, Customer>(properties);
 
